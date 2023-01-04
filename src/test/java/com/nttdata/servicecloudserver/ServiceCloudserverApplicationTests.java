a779ea5b3dbd68b82192f40e0028751ee599ef17
Wilder Darwin Vera Quispe <darvera@protonmail.com>
2023-01-04 00:05:09 -0500

    code initial

---
+++ src/test/java/com/nttdata/servicecloudserver/ServiceCloudserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdata.servicecloudserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceCloudserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
